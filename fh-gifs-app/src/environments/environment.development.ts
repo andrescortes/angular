78f3a949124403a0e888e01d90c9c06f496f0e0d
Miguel Cortes <andresabyac@gmail.com>
2025-09-13 23:13:40 -0500

    credentials for api

--- fh-gifs-app/src/environments/environment.development.ts
+++ fh-gifs-app/src/environments/environment.development.ts
@@ -3,4 +3,6 @@ export const environment = {
   companyLabel: 'Gifs',
   companyLabel2: 'App',
   companySubLabel: 'Handle your Gifs',
+  GIPHY_URL_BASE: 'https://api.giphy.com/v1',
+  GIPHY_KEY: 'test'
 };
