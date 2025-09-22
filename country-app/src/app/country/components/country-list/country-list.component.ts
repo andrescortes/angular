8a6c7200f57b48ef7876f762b709a2a9f8cef1c4
Miguel Cortes <andresabyac@gmail.com>
2025-09-21 22:12:53 -0500

    activated redirection for new component country-page

--- country-app/src/app/country/components/country-list/country-list.component.ts
+++ country-app/src/app/country/components/country-list/country-list.component.ts
@@ -1,10 +1,11 @@
 import { DecimalPipe } from '@angular/common';
 import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { ICountry } from '@country/country.interface';
 
 @Component({
   selector: 'app-country-list',
-  imports: [DecimalPipe],
+  imports: [ DecimalPipe, RouterLink ],
   templateUrl: './country-list.component.html',
   styleUrl: './country-list.component.css'
 })
