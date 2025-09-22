842e8d1853e4fd50fefcbec76999591f84025832
Miguel Cortes <andresabyac@gmail.com>
2025-09-21 21:54:36 -0500

    using resource from angular

--- country-app/src/app/country/pages/by-country-page/by-country-page.component.ts
+++ country-app/src/app/country/pages/by-country-page/by-country-page.component.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
 
 import { CountryListComponent } from '@country/country-list/country-list.component';
+import { CountryService } from '@country/country.service';
 import { SearchInputComponent } from '@country/search-input/search-input.component';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-by-country-page',
-  imports: [CountryListComponent, SearchInputComponent],
+  imports: [ CountryListComponent, SearchInputComponent ],
   templateUrl: './by-country-page.component.html',
   styleUrl: './by-country-page.component.css'
 })
 export class ByCountryPageComponent {
+  private readonly countryService = inject(CountryService);
 
-  onChange(value: string) {
-    console.log('value :>> ', value);
-  }
+  query = signal<string>('');
 
+  countryResource = resource({
+    request: () => ({ query: this.query() }),
+    loader: async ({ request }) => {
+      if (!request.query) {
+        return [];
+      }
+      return await firstValueFrom(this.countryService.searchByQueryAndPath(request.query,'name'))
+    }
+  });
 }
--- country-app/src/app/country/pages/by-region-page/by-region-page.component.ts
+++ country-app/src/app/country/pages/by-region-page/by-region-page.component.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
 
 import { CountryListComponent } from '@country/country-list/country-list.component';
+import { CountryService } from '@country/country.service';
 import { SearchInputComponent } from '@country/search-input/search-input.component';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-by-region-page',
-  imports: [SearchInputComponent, CountryListComponent],
+  imports: [ SearchInputComponent, CountryListComponent ],
   templateUrl: './by-region-page.component.html',
   styleUrl: './by-region-page.component.css'
 })
 export class ByRegionPageComponent {
+  private readonly countryService = inject(CountryService);
+  query = signal<string>('');
 
-  onChange(value: string) {
-    console.log('value :>> ', value);
-  }
+  countryResource = resource({
+    request: () => ({
+      query: this.query()
+    }),
+    loader: async ({ request }) => {
+      if (!request.query) return [];
+      return await firstValueFrom(this.countryService.searchByQueryAndPath(request.query, 'region'));
+    }
+  });
 
 }
