78dbfe09f8b48c788bbfb62f2caaa7c2b0b78372
Miguel Cortes <andresabyac@gmail.com>
2025-09-26 08:19:44 -0500

    feat: add some pipes implementations

--- fh-pipes-app/src/app/pipes/pages/uncommon-page/uncommon-page.component.ts
+++ fh-pipes-app/src/app/pipes/pages/uncommon-page/uncommon-page.component.ts
@@ -1,11 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CardComponent } from "../../components/card/card.component";
+import { I18nPluralPipe, I18nSelectPipe, JsonPipe, KeyValuePipe, NgIf, SlicePipe, UpperCasePipe } from '@angular/common';
+
+const client = {
+  name: 'Fernando',
+  age: 35,
+  gender: 'male',
+  address: 'Ottawa, Canada'
+}
+
+const client2 = {
+  name: 'Melissa',
+  age: 30,
+  gender: 'female',
+  address: 'Toronto, Canada'
+}
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [],
+  imports: [ CardComponent, I18nSelectPipe, I18nPluralPipe, SlicePipe, NgIf, JsonPipe, KeyValuePipe, UpperCasePipe ],
   templateUrl: './uncommon-page.component.html',
   styleUrl: './uncommon-page.component.css'
 })
 export class UncommonPageComponent {
 
+  // i18n Select
+  client = signal(client);
+  client2 = signal(client2);
+
+  invitationMap = {
+    male: 'Mr.',
+    female: 'Ms.'
+  }
+
+  changeClient(): void {
+    if (this.client() === client) {
+      this.client.set(client2);
+
+    } else {
+      this.client.set(client);
+    }
+  }
+
+  clientsMapThere = signal({
+    '=0': 'There is',
+    '=1': 'There is',
+    other: 'There are'
+  });
+
+  clientsMap = signal({
+    '=0': ' no clients',
+    '=1': ' one client',
+    other: ' clients'
+  });
+
+
+  // i18n Plural
+  clients = signal([
+    'Fernando',
+    'Melissa',
+    'Juan',
+    'Pedro',
+    'Maria',
+    'John'
+  ]);
+
+  removeClient(): void {
+    this.clients.update(cl => cl.slice(1));
+  }
+
+  get keys(): string[] {
+    return Object.keys(client);
+  }
+
+  get values() {
+    return Object.values(client);
+  }
 }
