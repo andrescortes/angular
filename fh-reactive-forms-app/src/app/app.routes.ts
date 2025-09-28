09ae88804642b3983d297c808c458b93599da989
Miguel Cortes <andresabyac@gmail.com>
2025-09-28 06:43:21 -0500

    feat: add pages, routes and basic forms

--- fh-reactive-forms-app/src/app/app.routes.ts
+++ fh-reactive-forms-app/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'reactive',
+        loadChildren: () => import('./forms/reactive/reactive.routes').then(m => m.reactiveRoutes)
+    },
+    {
+        path: 'auth',
+        loadChildren: () => import('./forms/auth/auth.routes').then(m => m.authRoutes)
+    },
+    {
+        path: 'country',
+        loadChildren: () => import('./forms/country/country.routes').then(m => m.countryRoutes)
+    },
+    {
+        path: '**',
+        redirectTo: 'reactive'
+    }
+];
---
+++ fh-reactive-forms-app/src/app/forms/country/country.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { CountryPageComponent } from "./pages/coutry-page/country-page.component";
+
+export const countryRoutes: Routes = [
+    {
+        path: '',
+        component: CountryPageComponent,
+    },
+]
