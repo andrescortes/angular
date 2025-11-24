c6617a506d1ebd16e6ee1717099619f007827973
Miguel Cortes <andresabyac@gmail.com>
2025-11-23 20:18:06 -0500

    feat(forms): implement reactive form with validation and dynamic side menu

--- fh-reactive-forms-app/src/app/forms/reactive/pages/basic-page/basic-page.component.ts
+++ fh-reactive-forms-app/src/app/forms/reactive/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,85 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.component.html',
-  styleUrl: './basic-page.component.css'
+  styleUrl: './basic-page.component.css',
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
+  private readonly fb = inject(FormBuilder);
+  errors: Map<string, string> = new Map();
 
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStock: [0, [Validators.required, Validators.min(0)]],
+  });
+
+  public get name(): AbstractControl {
+    return this.myForm.controls['name'];
+  }
+
+  public get price(): AbstractControl {
+    return this.myForm.controls['price'];
+  }
+
+  public get inStock(): AbstractControl {
+    return this.myForm.controls['inStock'];
+  }
+
+  ngOnInit(): void {
+    this.onChangesForm();
+  }
+
+  isValidField(field: string): boolean {
+    const formField = this.myForm.get(field);
+    if (!formField?.errors) return false;
+
+    console.log('formField.errors :>> ', formField.errors);
+    const hasError =
+      formField.errors['required'] || formField.errors['minlength'];
+    return hasError && this.myForm.controls[field].touched;
+  }
+
+  onChangesForm(): void {
+    this.myForm.valueChanges.subscribe(({ name, price, inStock }) => {
+      console.log({ name, price, inStock });
+      this.updateErrors(this.myForm, this.errors);
+    });
+  }
+
+  updateErrors(form: FormGroup, errors: Map<string, string>): void {
+    errors.clear();
+    Object.keys(form.controls).forEach((key) => {
+      const controlErrors = form.get(key)?.errors;
+      if (controlErrors) {
+        Object.keys(controlErrors).forEach((keyError) => {
+          errors.set(key, this.validationMessage(key, keyError));
+        });
+      }
+    });
+    console.log('errors :>> ', errors);
+  }
+
+  validationMessage(field: string, keyError: string): string {
+    switch (keyError) {
+      case 'required':
+        return `${field} es requerido`;
+      case 'minlength':
+        return `${field} debe de tener mínimo 3 caracteres`;
+      default:
+        break;
+    }
+    return '';
+  }
 }
--- fh-reactive-forms-app/src/app/shared/side-menu/side-menu.component.ts
+++ fh-reactive-forms-app/src/app/shared/side-menu/side-menu.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { reactiveRoutes } from '../../forms/reactive/reactive.routes';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuItem {
+  title: string;
+  route: string;
+}
+
+const reactiveRoute = reactiveRoutes[0].children ?? [];
 
 @Component({
   selector: 'app-side-menu',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu.component.html',
-  styleUrl: './side-menu.component.css'
+  styleUrl: './side-menu.component.css',
 })
 export class SideMenuComponent {
+  reactiveMenu: MenuItem[] = reactiveRoute
+    .filter((route) => route.path !== '**')
+    .map((route) => ({
+      page: 'Reactive',
+      title: `${route.title}`,
+      route: `reactive/${route.path}`,
+    }));
 
+  authMenu: MenuItem[] = [
+    {
+      title: 'Register',
+      route: './auth',
+    },
+  ];
+
+  countryMenu: MenuItem[] = [
+    {
+      title: 'Countries',
+      route: './country',
+    },
+  ];
 }
